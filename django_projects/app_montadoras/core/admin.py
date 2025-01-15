b6efd8f6e47eb152e7ea99036f5a471a600eefd5
Lyan Kaleu <lyankaleu12@gmail.com>
2025-01-15 15:33:49 -0300

    config: alterar models e admin Django

--- django_projects/app_montadoras/core/admin.py
+++ django_projects/app_montadoras/core/admin.py
@@ -1,19 +1,30 @@
 from django.contrib import admin
-from core.models import Montadora, ModeloVeiculo
+from core.models import Montadora, ModeloVeiculo, Veiculos
+
+
+class ModeloInline(admin.TabularInline):
+    model = ModeloVeiculo
+    extra = 0
+    fields = ('nome', 'motorizacao', 'em_producao')
+    show_change_link = True
 
 
 @admin.register(Montadora)
 class MontadoraAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'pais', 'ano_fundacao')
+    list_display = ('nome', 'quantidade_veiculos', 'pais', 'ano_fundacao', 'idade')
     search_fields = ('nome', 'pais')
     list_filter = ('pais',)
+
+    inlines = [ModeloInline]
 
 
 @admin.register(ModeloVeiculo)
 class ModeloVeiculoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'motorizacao',)
-    search_fields = ('nome', 'motorizacao')
-    list_filter = ('motorizacao',)
+    list_display = ('nome', 'motorizacao', 'montadora')
+    list_filter = ('motorizacao', 'montadora')
 
 
-# admin.site.register(Montadora, MontadoraAdmin)
+@admin.register(Veiculos)
+class VeiculosAdmin(admin.ModelAdmin):
+    list_display = ('modelo', 'cor', 'ano', 'km_rodados', 'placa')
+    list_filter = ('modelo',)
--- django_projects/app_montadoras/core/models.py
+++ django_projects/app_montadoras/core/models.py
@@ -1,14 +1,29 @@
+from django.contrib import admin
 from django.db import models
+import datetime
 
 
 class Montadora(models.Model):
-    nome = models.CharField(max_length=128, blank=False, null=False)
-    pais = models.CharField(max_length=128, blank=False, null=False)
-    ano_fundacao = models.PositiveIntegerField()
+    nome = models.CharField(verbose_name='Nome', max_length=128, blank=False, null=False)
+    pais = models.CharField(verbose_name='País', max_length=128, blank=False, null=False)
+    ano_fundacao = models.PositiveIntegerField(verbose_name='Ano de Fundação')
+
+    class Meta:
+        verbose_name = 'Montadora'
+        verbose_name_plural = 'Montadoras'
+        ordering = ['nome']
 
     def __str__(self):
         return f'{self.nome} - {self.pais}'
+    
+    @admin.display(description='Anos de operação')
+    def idade(self):
+        return datetime.date.today().year - self.ano_fundacao
 
+    @admin.display(description='Qtd Veículos')
+    def quantidade_veiculos(self):
+        return self.veiculos.count()
+    
 
 MOTOR_CHOICES = (
     ('1.0', 'Motor 1.0'),
@@ -22,9 +37,31 @@ MOTOR_CHOICES = (
 class ModeloVeiculo(models.Model):
     nome = models.CharField(max_length=128, null=False, blank=False)
     motorizacao = models.CharField(max_length=5, choices=MOTOR_CHOICES, null=False, blank=False)
+    em_producao = models.BooleanField(default=False, blank=False, null=False)
 
-    # Relacionamento
-    motandora = models.ForeignKey(Montadora, on_delete=models.RESTRICT, related_name='veiculos')
+    # Relacionamento (Many-to-One)
+    montadora = models.ForeignKey(Montadora, on_delete=models.RESTRICT, related_name='veiculos')
+
+    class Meta:
+        verbose_name = 'Modelo de Veículo'
+        verbose_name_plural = 'Modelos de Veículos'
+        ordering = ['montadora', 'nome']
 
     def __str__(self):
-        return self.nome
+        return f'{self.montadora} - {self.nome}'
+
+
+class Veiculos(models.Model):
+    placa = models.CharField(max_length=7, null=False, blank=False)
+    cor = models.CharField(max_length=32, null=False, blank=False)
+    ano = models.PositiveIntegerField(null=False, blank=False)
+    modelo = models.ForeignKey(ModeloVeiculo, on_delete=models.RESTRICT, related_name='veiculos')
+    km_rodados = models.PositiveIntegerField(null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Veículo'
+        verbose_name_plural = 'Veículos'
+        ordering = ['modelo__montadora__nome', 'modelo__nome']
+
+    def __str__(self):
+        return f'{self.modelo} - {self.placa}'
