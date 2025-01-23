ce444466d273a1953d5a4b55d395a18f80adf45a
Lyan Kaleu <lyankaleu12@gmail.com>
2025-01-22 21:38:29 -0300

    config: alterar fuso horário e personalizar cabeçalho do admin

--- django_projects/app_projetos/app_projetos/urls.py
+++ django_projects/app_projetos/app_projetos/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+admin.site.site_header = 'R1 Project Management'
+admin.site.index_title = 'Operações'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
