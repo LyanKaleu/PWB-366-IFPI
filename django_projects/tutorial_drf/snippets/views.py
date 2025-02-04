1dedf5374be0ef7bec770a565494d6a85e90f50f
Lyan Kaleu <lyankaleu12@gmail.com>
2025-02-03 21:53:45 -0300

    add tutorial-drf from django-rest-framework.org

---
+++ django_projects/tutorial_drf/snippets/views.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework import generics, permissions
+
+
+class SnippetList(generics.ListCreateAPIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class UserList(generics.ListAPIView):
+    """
+    List all users.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    """
+    Retrieve a user instance.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
